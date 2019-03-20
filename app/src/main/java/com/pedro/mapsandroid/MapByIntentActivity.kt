c56931c09c71cddba6097e3ac225a6ad4be0ad2b
LeAlbus <pedrolopes.dev@gmail.com>
2019-03-20 19:56:54 -0300

    Repo creation

---
+++ app/src/main/java/com/pedro/mapsandroid/MapByIntentActivity.kt
@@ -0,0 +1,12 @@
+package com.pedro.mapsandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MapByIntentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_by_intent)
+    }
+}
